b1a231faf2400537e5898e173e7c41b608e8090d
4Columns-Jordan <jordan@fourcolumns.net>
2024-04-04 14:28:14 -0500

    Updated version number

--- next-post.php
+++ next-post.php
@@ -3,7 +3,7 @@
 Plugin Name: Next Post
 Plugin URI: https://fourcolumns.net
 Description: This plugin adds next and previous buttons on the posts.
-Version: 1.0.0
+Version: 1.0.1
 Author: The Four Columns Team
 Author URI: https: //fourcolumns.net
 License: GPLv2
