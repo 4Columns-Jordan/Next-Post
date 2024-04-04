16269db6649f3c84483af8b0c246627a3cd47578
4Columns-Jordan <jordan@fourcolumns.net>
2024-04-04 11:36:52 -0500

    Built out getPostUrl function

--- next-post.php
+++ next-post.php
@@ -22,7 +22,16 @@ function FCNP__addNextButton(){
 }
 add_action('genesis_after_entry_content', 'FCNP__addNextButton');
 
-function FCNP__getPostUrl($id = null, $direction = 1){
+/**
+ * This function gets the next or previous post based on the
+ * supplied ID, and direction parameter. Defaults to next post.
+ * 
+ *  @param $id - INT - Current post ID
+ *  @param $direction - Direction of the query, defaults to after
+ *  @return null or STR - Returns null if no posts or no id param,
+ *  otherwise ir returns a string of the post url
+ */
+function FCNP__getPostUrl($id = null, $direction = 'after'){
   if(!isset($id)) return;
   // setting up the query
   $queryArgs = array(
@@ -30,12 +39,33 @@ function FCNP__getPostUrl($id = null, $direction = 1){
     'posts_per_page' => 1,
     'orderby' => 'date',
     'order' => 'ASC',
-    'date_query' => array(
+    'post__not_in' => array($id),
+  );
+  // setting the date query, if the direction variable exists
+  // and it's a string
+  if(!empty($direction) && is_string($direction)){
+    // set the date query with the direction param
+    $queryArgs['date_query'] = array(
       array(
-        'after' => get_the_date('Y-m-d H:i:s', $id),
-      ),
+        $direction => get_the_date('Y-m-d H:i:s', $id),
     ),
   );
-
-  return $id;
+  }
+  // getting the private category
+  $privateCat = get_category_by_slug('private');
+  // if the private category exists, exclude it from the query
+  if(!empty($privateCat))
+    $queryArgs['category__not_in'] = $privateCat->term_id;
+  // making a new query
+  $postQuery = new WP_Query($queryArgs);
+  // if the query is not empty
+  if(!empty($postQuery->posts)){
+    // get the post id
+    $postID = $postQuery->posts[0]->ID;
+    // get the permalink
+    $permaLink = get_the_permalink($postID);
+    return $permaLink;
+  }
+  // return null if there are no posts
+  return null;
 }
