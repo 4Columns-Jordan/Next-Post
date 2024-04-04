b745221a072a0d8f08452108cfc41464191d9136
4Columns-Jordan <jordan@fourcolumns.net>
2024-04-04 10:37:18 -0500

    Init

---
+++ next-post.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Next Post
+Plugin URI: https://fourcolumns.net
+Description: This plugin adds next and previous buttons on the posts.
+Version: 0.0.1
+Author: The Four Columns Team
+Author URI: https: //fourcolumns.net
+License: GPLv2
+*/
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Next Post
+This simple plugin adds previous and next post buttons to the single post pages.
